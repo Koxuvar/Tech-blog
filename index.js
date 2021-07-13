ca56563c388fc6426ebb381c9b92b736c6c8a690
MMVHome <connortsullivan1@gmail.com>
2021-07-12 22:18:02 -0500

    heroku 4

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ seeds.seedAll();
 
 app.use(routes);
 
-sequelize.sync({force: false}).then(() =>
+sequelize.sync({force: true}).then(() =>
 {
     app.listen(PORT, () => console.log('Now Listening'))
 });
